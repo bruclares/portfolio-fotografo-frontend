48b171e957ee702842ce9838d054497423449e87
Bruna Clares Cardoso <clares.bruna@gmail.com>
2025-02-12 13:34:33 -0300

    style: refatora estilos globais e adiciona novos estilos para tipografia e componentes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-const toggleMenuButtons = document.querySelectorAll(".toggle-menu");
+const toggleMenuButton = document.querySelector(".toggle-menu");
 
-toggleMenuButtons.forEach((botao) =>
-  botao.addEventListener("click", function () {
-    const menu = document.querySelector(".menu");
-    menu.classList.toggle("show");
-  })
-);
+toggleMenuButton.addEventListener("click", function () {
+  const menu = document.querySelector(".menu-links");
+  menu.classList.toggle("show");
+  toggleMenuButton.innerText =
+    toggleMenuButton.innerText == "Menu" ? "Fechar" : "Menu";
+  document.body.classList.toggle("menu-open");
+
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("text-secondary");
+  navbar.classList.toggle("text-highlight");
+});
