694df829554713dda15247cc69dbea5abd8e8fe7
Bruna Clares Cardoso <clares.bruna@gmail.com>
2025-05-24 07:46:39 -0300

    style(js): padroniza uso de aspas e formatação
    - Substitui aspas duplas por aspas simples
    - Ajusta estilo dos seletores e condições
    - Melhora clareza e manutenção do script principal

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const toggleMenuButton = document.querySelector(".toggle-menu");
-const navbar = document.querySelector(".navbar");
+const toggleMenuButton = document.querySelector('.toggle-menu');
+const navbar = document.querySelector('.navbar');
 const corPadrao = Array.from(navbar.classList).find((classe) =>
-  classe.includes("text-")
+  classe.includes('text-')
 );
 
-toggleMenuButton.addEventListener("click", function () {
-  const menu = document.querySelector(".menu-links");
-  menu.classList.toggle("show");
+toggleMenuButton.addEventListener('click', function () {
+  const menu = document.querySelector('.menu-links');
+  menu.classList.toggle('show');
   toggleMenuButton.innerText =
-    toggleMenuButton.innerText == "Menu" ? "Fechar" : "Menu";
-  document.body.classList.toggle("menu-open");
+    toggleMenuButton.innerText == 'Menu' ? 'Fechar' : 'Menu';
+  document.body.classList.toggle('menu-open');
 
   navbar.classList.toggle(corPadrao);
-  navbar.classList.toggle("text-secondary");
+  navbar.classList.toggle('text-secondary');
 });
